3977933c69550875e6d3c619321f97c9ae974afb
Zerotask <hintermayer.patrick@gmail.com>
2020-09-12 12:58:19 +0200

    feat: added course entity

---
+++ MyUniversity/Data/Entities/Course.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MyUniversity.Data.Entities
+{
+    public class Course : IEntity
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(5)]
+        [StringLength(100)]
+        public string Name { get; set; }
+
+        [Required]
+        [MinLength(10)]
+        [StringLength(1000)]
+        public string Description { get; set; }
+
+        [Required]
+        public DateTime LastUpdate { get; set; }
+
+        [Description("Number of students attending this course.")]
+        public int CountStudents { get; set; }
+    }
+}
